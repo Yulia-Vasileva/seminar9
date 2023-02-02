85027431911cce1fad1d4d22910bff698857958d
Yulia <vasilevaa93@mail.ru>
2023-02-02 07:25:34 +0300

    задача64

---
+++ задача64/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+void PrintNumbers(int n)
+{
+Console.Write(n+" ");
+    if(n==1) return;
+PrintNumbers(n-1);
+}
+int number=7;
+PrintNumbers(number);
